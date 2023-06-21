0974f811c36f96b46110203d3e79d95d36fe43a4
Shivam Kesarwani <shivamkesarwani001@gmail.com>
2023-06-21 14:28:31 +0530

    Added controller/user_controller.js,routes/ users.js

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,9 @@ const router=express.Router()
 const homeController=require('../controllers/home_controller')
 
 router.get('/', homeController.home);
+router.use('/users', require('./users'));
+
+//for any further routes, access from here
+//router.use('/routerName', require('/routerFile'))
 
 module.exports=router;
