28ec04eee8a19dfbf9f748b90dfaf4492e6fdf51
Shivam Kesarwani <shivamkesarwani001@gmail.com>
2023-06-21 14:38:05 +0530

    Added routes/posts.js and controllers/post_controller.js

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,11 @@ const express=require('express')
 const router=express.Router()
 
 const homeController=require('../controllers/home_controller')
+const postController=require('../controllers/post_controller')
 
 router.get('/', homeController.home);
 router.use('/users', require('./users'));
-
+router.use('/posts', postController.post);
 //for any further routes, access from here
 //router.use('/routerName', require('/routerFile'))
 
