8e17235c3437197c8c25e490bcda4dafb4f0c805
Shivam Kesarwani <shivamkesarwani001@gmail.com>
2023-06-24 01:00:27 +0530

    Implemented controller functions for createSession on signin, show details on sigin in views, and signout functionality

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,7 +1,19 @@
 const User=require('../models/user')
 
-module.exports.profile=function(req, res){
-    return res.render('user_profile')
+module.exports.profile=async function(req, res){
+    console.log(`4. Cookie: ${req.cookies.user_id}`)
+    if(req.cookies.user_id){
+        const user=await User.findById(req.cookies.user_id)
+
+        if(user){
+            return res.render('user_profile', {title: 'Codial | Profile', user: user})
+        }
+        return res.redirect('/users/signin')
+    }
+    else{
+        return res.redirect('/users/signin')
+    }
+   
 }
 
 
@@ -13,17 +25,16 @@ module.exports.signin=function(req, res){
     return res.render('user_signin', {title: 'Codial | Sign In'})
 }
 
-module.exports.create=function(req, res){
+module.exports.create=async function(req, res){
     if(req.body.password != req.body.confirm_password){
         return res.redirect('back');
     }
 
-    const user=User.findOne({email: req.body.email});
-    user
-    .then((data)=>{
-        console.log('DATA:',data)
+    const user=await User.findOne({email: req.body.email});
+
+        console.log('DATA:',user)
         console.log('Req Data:', req.body)
-        if(!data){        
+        if(!user){        
             User.create(req.body)
             .then(u=>{
                 console.log(`Then u: ${u}`)
@@ -37,12 +48,39 @@ module.exports.create=function(req, res){
             console.log('User already exists')
             res.redirect('back')
         }
-    })
-    .catch(err=>{
-        console.log('Error in finding user in signing up')
-    })
 }
 
-module.exports.createSession=function(req, res){
-    return res.render('user_signin', {title: 'Codial | Sign In'})
+module.exports.createSession=async function(req, res){
+    //find the user
+    console.log(`Email: ${req.body.email}`)
+    const user= await User.findOne({email: req.body.email})
+
+    console.log(`51. User: ${user}`)
+
+        console.log(`54. User: ${user}`)
+        console.log(`55. Body: ${req.body}`)
+        //handle user found
+        if(user){
+            //handle password which doesnt match
+            if(user.password!=req.body.password){
+                return res.redirect('back');
+            }
+            //handle session creation
+            res.cookie('user_id', user.id);
+            console.log(`64, ${req.cookies.user_id}`)
+            return res.redirect('/users/profile')
+        }
+        else{
+            //handle user not found
+            return res.redirect('back');
+        }
+        // console.log(`76 Error in finding user in sign in`);
+        // return res.redirect('back');
+}
+
+
+module.exports.signout=function(req, res){
+    console.log(`Signout: ${req.cookies.user_id}`)
+    res.clearCookie('user_id')
+    return res.redirect('/users/signin')
 }
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router=express.Router()
-
+const User=require('../models/user')
 const userController=require('../controllers/user_controller')
 
 router.get('/profile', userController.profile)
@@ -8,4 +8,6 @@ router.get('/profile', userController.profile)
 router.get('/signup', userController.signup)
 router.get('/signin', userController.signin)
 router.post('/create', userController.create)
+router.post('/create-session', userController.createSession)
+router.get('/signout', userController.signout)
 module.exports=router
